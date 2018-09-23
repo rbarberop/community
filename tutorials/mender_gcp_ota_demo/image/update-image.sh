b19524e054f3189f806b3267de7bd5aac9c1e3ea
Preston Holmes <preston@ptone.com>
2018-09-23 09:58:33 -0700

    Guard against missing project env

--- tutorials/mender_gcp_ota_demo/image/update-image.sh
+++ tutorials/mender_gcp_ota_demo/image/update-image.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
+
+if [ -z "${PROJECT}" ]; then
+  echo "You need to set PROJECT env var"
+  exit 1
+fi
+
 wget https://d1b0l86ne08fsf.cloudfront.net/mender-artifact/2.3.0/mender-artifact
 chmod +x mender-artifact
 wget https://storage.googleapis.com/mender-gcp-ota-images/gcp-mender-demo-image-raspberrypi3.sdimg
@@ -11,7 +17,10 @@ export REGION_ID=us-central1
 export REGISTRY_ID=mender-demo
 ...
 EOF
+echo "sample image download complete, Congure image with your project setting..."
 cat ./gcp-config.sh | ./mender-artifact cp gcp-mender-demo-image-raspberrypi3.sdimg:/data/gcp/gcp-config.sh
+echo "Image configured, uploading to bucket..."
 gsutil cp gcp-mender-demo-image-raspberrypi3.sdimg gs://$PROJECT-mender-builds/updated-demo-image-raspberrypi3.img
 rm gcp-mender-demo-image-raspberrypi3.sdimg
+echo "Image upload complete"
 echo "Retrieve image with gsutil cp gs://${PROJECT}-mender-builds/updated-demo-image-raspberrypi3.img ."
